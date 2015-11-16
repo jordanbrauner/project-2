2036e14f4c44c6b7316e2ec2d95b7422d1931a13
Jordan Brauner <jordan.brauner@gmail.com>
2015-11-16 11:58:27 -0500

    Further styling and adding to the home page

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,13 +1,11 @@
 class RecipesController < ApplicationController
-  before_action :set_post, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
   def index
-
+    @recipes = Recipe.all
   end
 
-  # private
-  #   def recipe_params
-  #     params.require(:recipe).permit(:title, :image_url)
-  #   end
+  private
+    def recipe_params
+      params.require(:recipe).permit(:title, :image_url)
+    end
 end
