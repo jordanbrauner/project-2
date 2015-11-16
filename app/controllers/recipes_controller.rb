726abcf3f4b7a9630ebc2afaa20f0e9525434729
Jordan Brauner <jordan.brauner@gmail.com>
2015-11-16 12:57:05 -0500

    More styling and structure for the home page

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -4,8 +4,43 @@ class RecipesController < ApplicationController
     @recipes = Recipe.all
   end
 
+  def show
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+    if @recipe.save
+      flash[:notice] = "#{@recipe.title} was created."
+      redirect_to @recipe
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def update
+    @recipe = Recipe.find(params[:id])
+    if @recipe.update(recipe_params)
+      flash[:notice] = "#{@recipe.title} was updated."
+      redirect_to @recipe
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+  end
+
   private
     def recipe_params
-      params.require(:recipe).permit(:title, :image_url)
+      params.require(:recipe).permit(:title, :description, :photo_url, :ingredients, :directions)
     end
 end
