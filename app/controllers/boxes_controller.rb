a714539e7cfbfd24c26882142f8391159893c0e3
Jordan Brauner <jordan.brauner@gmail.com>
2015-11-18 16:44:59 -0500

    Styled boxes page

--- app/controllers/boxes_controller.rb
+++ app/controllers/boxes_controller.rb
@@ -4,6 +4,7 @@ class BoxesController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
   def index
+    @user = current_user
   end
 
   def show
