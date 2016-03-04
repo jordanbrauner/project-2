04eb1339c3b4bd06e680dba7689152ba69aa6aca
Jordan Brauner <jordan.brauner@gmail.com>
2016-03-04 09:05:54 -0500

    Edit and delete account added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,13 +8,25 @@ class UsersController < ApplicationController
     @contributor_latest = @contributor.recipes.last
   end
 
+  def edit
+  end
+
+  def update
+    if @user.update!(user_params)
+      flash[:notice] = "#{@user.username} was updated."
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
+
   def user_vars
     @user = current_user
     @recipes = Recipe.all
   end
 
   private
-  def recipe_params
-    params.require(:recipe).permit(:title, :description, :photo_url, :ingredients, :directions)
+  def user_params
+    params.require(:user).permit(:email, :username, :photo_url)
   end
 end
